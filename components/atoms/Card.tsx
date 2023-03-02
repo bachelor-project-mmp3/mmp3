6a43ec0f9f6e88339016e6e2a198bbecdd2e1488
Tanja Santner <tsantner.mmt-b2020@fh-salzburg.ac.at>
2023-03-02 09:47:34 +0100

    Add component card #4

--- components/atoms/Card.tsx
+++ components/atoms/Card.tsx
@@ -1,7 +1,29 @@
+import { ReactNode } from 'react';
+import styled from 'styled-components';
 import React from 'react';
 
-const Card: React.FC = () => {
-    return <div>TODO</div>;
+interface CardProps {
+    height?: string;
+    width?: string;
+    children: ReactNode;
+}
+
+export const Card: React.FC<CardProps> = ({
+    children,
+    height,
+    width,
+}: CardProps) => {
+    return (
+        <StyledCard style={{ height: height, width: width }}>
+            {children}
+        </StyledCard>
+    );
 };
 
-export default Card;
+const StyledCard = styled.div`
+    border-radius: 40px;
+    box-shadow: 17px 17px 35px -11px ${({ theme }) => theme.darkGrey};
+    padding: 10px 20px 20px 20px;
+    margin: 40px 0;
+    background: white;
+`;
