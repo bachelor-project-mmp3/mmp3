ab1b14b258d74315b42d91b37c0c34bec0f9e6b2
Tanja Santner <tsantner.mmt-b2020@fh-salzburg.ac.at>
2023-03-02 14:01:33 +0100

    Update card component #4

--- components/atoms/Card.tsx
+++ components/atoms/Card.tsx
@@ -3,27 +3,38 @@ import styled from 'styled-components';
 import React from 'react';
 
 interface CardProps {
+    variant?: 'no-padding';
     height?: string;
     width?: string;
     children: ReactNode;
 }
 
 export const Card: React.FC<CardProps> = ({
+    variant,
     children,
     height,
     width,
 }: CardProps) => {
     return (
-        <StyledCard style={{ height: height, width: width }}>
+        <StyledCard variant={variant} style={{ height: height, width: width }}>
             {children}
         </StyledCard>
     );
 };
 
-const StyledCard = styled.div`
+interface CardStyleProps {
+    variant: 'no-padding';
+}
+
+const StyledCard = styled.div<CardStyleProps>`
     border-radius: 40px;
     box-shadow: 17px 17px 35px -11px ${({ theme }) => theme.darkGrey};
-    padding: 10px 20px 20px 20px;
     margin: 40px 0;
     background: white;
+    padding: 10px 20px 20px 20px;
+    ${(props) =>
+        props.variant === 'no-padding' &&
+        `
+        padding: 0;
+    `}
 `;
