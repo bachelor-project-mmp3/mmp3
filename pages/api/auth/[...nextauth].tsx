01d7ddf26a363f502baa485199e751c0c59c4e7e
Tanja Santner <tsantner.mmt-b2020@fh-salzburg.ac.at>
2023-03-12 23:09:49 +0100

    Add default value for dormitory when user is created

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -41,6 +41,7 @@ export const authOptions: NextAuthOptions = {
                                 firstName: fetchedUser.given_name,
                                 lastName: fetchedUser.family_name,
                                 study: fetchedUser.studies.split('-')[0],
+                                dormitory: 'Campus Urstein',
                                 email: fetchedUser.email,
                                 image: 'https://firebasestorage.googleapis.com/v0/b/studentenfutter-dba6a.appspot.com/o/profile%2Fdefault-profile.jpg?alt=media&token=578a83b8-ef61-474c-8d9a-ee3b59af528f',
                             },
