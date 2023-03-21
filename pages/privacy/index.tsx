1f335a6a3b425d5b1f2259cd2a46a6ef9e629dbc
Tanja Santner <tsantner.mmt-b2020@fh-salzburg.ac.at>
2023-03-21 11:25:26 +0100

    Escape apostroph and quotation mark privacy policy #29

--- pages/privacy/index.tsx
+++ pages/privacy/index.tsx
@@ -118,11 +118,11 @@ const Privacy = () => {
                 <p>
                     Our website may contain links to other websites that are not
                     operated by us. If you click on a third party link, You will
-                    be directed to that third party's site. We strongly advise
-                    you to review the privacy policy of every site you visit. We
-                    have no control over and assume no responsibility for the
-                    content, privacy policies or practices of any third party
-                    sites or services.
+                    be directed to that third party&apos;s site. We strongly
+                    advise you to review the privacy policy of every site you
+                    visit. We have no control over and assume no responsibility
+                    for the content, privacy policies or practices of any third
+                    party sites or services.
                 </p>
                 <h2>Changes to this privacy policy</h2>
                 <p>
@@ -130,11 +130,11 @@ const Privacy = () => {
                     notify you of any changes by posting the new privacy policy
                     on this page. We will let you know via email and/or a
                     prominent notice on our website, prior to the change
-                    becoming effective and update the "Last updated" date at the
-                    top of this privacy policy. You are advised to review this
-                    privacy policy periodically for any changes. Changes to this
-                    privacy policy are effective when they are posted on this
-                    page.
+                    becoming effective and update the &quot;Last updated&quot;
+                    date at the top of this privacy policy. You are advised to
+                    review this privacy policy periodically for any changes.
+                    Changes to this privacy policy are effective when they are
+                    posted on this page.
                 </p>
                 <h2>Contact us</h2>
                 <p>
