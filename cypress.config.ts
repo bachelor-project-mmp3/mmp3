86cf31106b3feb6914298041a3f367a7a7955665
kerstin97 <kerstin.reichinger@gmx.at>
2023-05-23 22:18:48 +0200

    write e2e test for submission

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+        /*env: {
+            username: '', // fh usernae
+            password: '',  // fh password
+            firstname: '',  // profile firstname
+            id: '',  // your profile id
+        },*/
     },
-  },
 });
